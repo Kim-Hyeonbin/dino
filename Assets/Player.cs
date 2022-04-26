d72ee69bef8b6601446c806ae7dea73bbfe40124
KimHyeonbin <bin031001@gmail.com>
2022-04-26 20:46:18 +0900

    Implement player jump

---
+++ Assets/Player.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    private Rigidbody2D _rigidbody2D;
+    void Start()
+    {
+        _rigidbody2D = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            _rigidbody2D.AddForce(Vector2.up * 10, ForceMode2D.Impulse);
+        }
+    }
+}
